20cba212114a1604c457f9471910c5aa0d146436
kurumurthy pasupula <pasupula.kurumurthy04@gmail.com>
2026-09-22 21:17:33 +0530

    new variable added

--- app.py
+++ app.py
@@ -3,4 +3,6 @@
 def function(a,b,c):
     add = a + b + c
     subtract = a - b - c
-    return add, subtract
+    multiply = a * b * c
+    new  = a * b / c
+    return add, subtract, multiply, new
