b8509af2598b1b24638796e12c37bc1ff76cd777
kurumurthy pasupula <pasupula.kurumurthy04@gmail.com>
2026-09-22 21:23:12 +0530

    last code is removed

--- app.py
+++ app.py
@@ -4,5 +4,4 @@ def function(a,b,c):
     add = a + b + c
     subtract = a - b - c
     multiply = a * b * c
-    new  = a * b / c
-    return add, subtract, multiply, new
+    return add, subtract, multiply
